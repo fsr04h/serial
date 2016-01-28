e61775910c45d02cb7398fe2668667bce036a825
fsr04h <ionsgltos@gmail.com>
2016-01-28 14:57:07 +0000

    Update serial.py

--- serial.py
+++ serial.py
@@ -2,21 +2,17 @@ import serial
 import time
 import os
 
-#version3
 os.getcwd()
-ser = serial.Serial('/dev/ttyACM0', 114200, timeout = 5)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 5)
 
-file = open('samples2.csv')
+file = open('commands.csv')
 while 1:
- line = file.readline()
- if not line:
+ command = file.readline()
+ if not command:
         break
- ser.write(line)
- line2 = ser.readline()
- word1, word2, word3 = line2.split(' ') # Split on whitespace
- #print line3
- w0rd1, w0rd2 = word2.split(":")
- w0rd2 = float(w0rd2)
- print w0rd2
+ ser.write(command)
+
+ response = ser.readline()
+ print response
  time.sleep(5)
  file.close
